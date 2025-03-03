7f7404b78196e74389c144565bfaba45e8402e65
nkamalu620 <nkamalu96@gmail.com>
2025-03-02 21:53:12 -0500

    updated mutations.ts

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,21 +1,54 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!) {
-    addProfile(name: $name) {
-      _id
-      name
-      skills
+export const LOGIN_USER = gql`
+ mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
   }
 `;
 
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+       token
+      user {
+        _id
+        username
+        email
+      }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($title: String!, $author: String!) {
+    saveBook(title: $title, author: $author) {
       _id
-      name
-      skills
+      username
+      email
+      savedBooks {
+        _id
+        title
+        author
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      title
+      author
     }
   }
 `;
