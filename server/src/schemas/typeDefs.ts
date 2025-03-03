76cb0ff876e2e42156ffd050ee14687dd033f6e4
nkamalu620 <nkamalu96@gmail.com>
2025-03-02 21:23:23 -0500

    updated typeDefs.ts

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -2,13 +2,18 @@ const typeDefs = `
   type Book {
     _id: ID!
    title: String!
-   author: String!
+   author: [String]
+   description: String
+   image: String
+   link: String
   }
 
   type User {
     _id: ID!
     username: String!
     email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Query {
@@ -16,13 +21,21 @@ const typeDefs = `
     book(bookId: ID!): Book
     users: [User]!
     user(userId: ID!): User
+    me: User
   }
+
+type Auth {
+  token: ID!
+  user: User
+}
 
   # Important for useMutation: We define our Mutation type to inform our entrypoints
 
   type Mutation {
     addBook(title: String!, author: String!): Book
+    saveBook(title: String!, author: String!): User
     removeBook(bookId: ID!): Book
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!): User
     removeUser(userId: ID!): User
   }
